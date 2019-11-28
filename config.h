8dd1e31b9f8cf1aac538b38deb9a35bac5cbaf63
Wilyarti Howard <wilyarti@gmail.com>
2019-11-28 18:09:49 +1000

    Initial commit

---
+++ config.h
@@ -0,0 +1,15 @@
+// configuration options
+// esp8285 pin out 
+// GPIO 12 = D6
+// GPIO 14 = D5
+// GPIO 13 = D7
+// 15 =8
+// GPIO 4 = D2 (SDA)
+// GPIO 5 = D1 (SCL)
+// GPIO 2/3 = D4/RX
+
+#define CO2_RX 12
+#define CO2_TX 14
+
+#define SCL 5
+#define SDA 
